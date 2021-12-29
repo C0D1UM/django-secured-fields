52b5bcedb0ec31aa5f88159b817390f2c26deca1
Preeti Yuankrathok <preetisatit@gmail.com>
2021-12-29 17:43:13 +0700

    Fix path for django 3.1

--- test_secured_fields/main/settings.py
+++ test_secured_fields/main/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
