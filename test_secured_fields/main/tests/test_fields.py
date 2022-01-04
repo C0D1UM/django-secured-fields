ef8d51adcec5f77e493d34458c3d2dbb4784f866
Preeti Yuankrathok <preetisatit@gmail.com>
2022-01-04 20:44:25 +0700

    Reuse create assert function

--- test_secured_fields/main/tests/test_fields.py
+++ test_secured_fields/main/tests/test_fields.py
@@ -1,18 +1,25 @@
+import datetime
 import typing
-from unittest.case import skip
+
+import pytz
 from django import test
 from django.core import exceptions
 from django.db import connection
 from django.db.models import Model
+from django.utils import timezone
+from freezegun import freeze_time
 
 from main import models
+from main.tests import utils as test_utils
 from secured_fields.fernet import get_fernet
 
 
 class BaseTestCases:
-
     class BaseFieldTestCase(test.TestCase):
         model_class: typing.Type[Model]
+
+        def setUp(self) -> None:
+            self.model: typing.Optional[Model] = None
 
         def get_raw_field(self):
             with connection.cursor() as cursor:
@@ -30,6 +37,16 @@ class BaseTestCases:
 
             self.assertEqual(decrypted_value, expected_bytes)
 
+        def create_and_assert(self, create_value, assert_value: typing.Any = test_utils.NoValue, **extra_options):
+            extra_options = extra_options
+            if create_value is not test_utils.NoValue:
+                extra_options['field'] = create_value
+
+            self.model = self.model_class.objects.create(**extra_options)
+            # self.model.refresh_from_db()
+
+            self.assertEqual(self.model.field, assert_value if assert_value is not test_utils.NoValue else create_value)
+
         def test_null(self):
             model = self.model_class.objects.create(field=None)
 
@@ -41,9 +58,7 @@ class BinaryFieldTestCase(BaseTestCases.BaseFieldTestCase):
     model_class = models.BinaryFieldModel
 
     def test_simple(self):
-        model = self.model_class.objects.create(field=b'test')
-
-        self.assertEqual(model.field, b'test')
+        self.create_and_assert(b'test')
         self.assertEncryptedField(b'test')
 
 
@@ -51,9 +66,7 @@ class BooleanFieldTestCase(BaseTestCases.BaseFieldTestCase):
     model_class = models.BooleanFieldModel
 
     def test_simple(self):
-        model = self.model_class.objects.create(field=True)
-
-        self.assertEqual(model.field, True)
+        self.create_and_assert(True)
         self.assertEncryptedField(b'True')
 
 
@@ -61,14 +74,44 @@ class CharFieldTestCase(BaseTestCases.BaseFieldTestCase):
     model_class = models.CharFieldModel
 
     def test_simple(self):
-        model = self.model_class.objects.create(field='test')
-
-        self.assertEqual(model.field, 'test')
+        self.create_and_assert('test')
         self.assertEncryptedField(b'test')
 
     def test_exceed_max_length(self):
         model = self.model_class(field='1234567890123456789012345678901')
         self.assertRaises(exceptions.ValidationError, model.full_clean)
+
+
+class DateFieldTestCase(BaseTestCases.BaseFieldTestCase):
+    model_class = models.DateFieldModel
+
+    def test_simple(self):
+        self.create_and_assert(datetime.date(2021, 12, 31))
+        self.assertEncryptedField(b'2021-12-31')
+
+
+class DateTimeFieldTestCase(BaseTestCases.BaseFieldTestCase):
+    model_class = models.DateTimeFieldModel
+
+    @test.override_settings(USE_TZ=False)
+    def test_naive_no_use_tz(self):
+        self.create_and_assert(datetime.datetime(2021, 12, 31, 23, 59, 3))
+        self.assertEncryptedField(b'2021-12-31 23:59:03')
+
+    @test.override_settings(USE_TZ=True)
+    def test_naive_use_tz(self):
+        self.create_and_assert(datetime.datetime(2021, 12, 31, 23, 59, 3))
+        self.assertEncryptedField(b'2021-12-31 23:59:03+00:00')
+
+    @test.override_settings(USE_TZ=True)
+    def test_utc_use_tz(self):
+        self.create_and_assert(datetime.datetime(2021, 12, 31, 23, 59, 3, tzinfo=pytz.UTC))
+        self.assertEncryptedField(b'2021-12-31 23:59:03+00:00')
+
+    @test.override_settings(USE_TZ=True)
+    def test_bangkok_use_tz(self):
+        self.create_and_assert(timezone.localtime(datetime.datetime(2021, 12, 31, 23, 59, 3, tzinfo=pytz.UTC)))
+        self.assertEncryptedField(b'2021-12-31 23:59:03+00:00')
 
 
 class IntegerFieldTestCase(BaseTestCases.BaseFieldTestCase):
