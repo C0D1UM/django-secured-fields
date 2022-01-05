3dbbf11eae1812e1bc90ca74354a68b55f425ef6
Preeti Yuankrathok <preetisatit@gmail.com>
2022-01-05 16:02:38 +0700

    Remove comments

--- secured_fields/fields/__init__.py
+++ secured_fields/fields/__init__.py
@@ -23,11 +23,6 @@ class EncryptedBinaryField(mixins.EncryptedMixin, models.BinaryField):
     def prepare_encryption(self, value) -> bytes:
         return value
 
-    # def get_db_prep_value(self, value, connection, prepared=False):
-    #     if prepared:
-    #         value = super().get_prep_value(value)
-    #     return super().get_db_prep_value(value, connection, prepared)
-
 
 class EncryptedBooleanField(mixins.EncryptedMixin, models.BooleanField):
     pass
@@ -47,10 +42,6 @@ class EncryptedDateTimeField(mixins.EncryptedMixin, models.DateTimeField):
 
 class EncryptedDecimalField(mixins.EncryptedMixin, models.DecimalField):
     pass
-
-    # def get_db_prep_save(self, value, connection):
-    #     value = super().get_db_prep_save(value, connection)
-    #     return self.get_db_prep_value(value, connection, prepared=False)
 
 
 class EncryptedIntegerField(mixins.EncryptedMixin, models.IntegerField):
